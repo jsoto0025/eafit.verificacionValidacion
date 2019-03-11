860fd542be5c6cc76fa7def6bf49bdbc3a798049
totemapps <totemapps@outlook.com>
2019-03-10 21:43:27 -0500

    Proyecto de pruebas con Scripts

---
+++ TestScripting/SeleniumTestA/MainTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTestA
+{
+    [TestClass]
+    public class MainTest
+    {
+
+        ChromeDriver chromeDriver;
+
+        [TestInitialize]
+        public void TestInitialice()
+        {
+            chromeDriver = new ChromeDriver();
+            chromeDriver.Url = "https://www.alkosto.com";
+
+            //var ventana = chromeDriver.FindElementByClassName("grow");
+            //ventana.Click();
+        }
+
+        [TestMethod]
+        [Priority(0)]
+        public void LoginTest()
+        {
+            
+            var btnMyAccount = chromeDriver.FindElementByClassName("my-account-link");
+
+            btnMyAccount.Click();
+
+            var txtLogin = chromeDriver.FindElementById("login-username");
+
+            var txtPassword = chromeDriver.FindElementById("login-password");
+
+            txtLogin.SendKeys("jsoto25@hotmail.com");
+
+            txtPassword.SendKeys("indigo2");
+
+            var btnContinuar = chromeDriver.FindElementById("send2");
+
+            btnContinuar.Click();
+            
+        }
+
+        [TestMethod]
+        [Priority(1)]
+        public void LogoutTest()
+        {
+            
+            var btnLogout = chromeDriver.FindElementByClassName("log-out-link");
+            btnLogout.Click();
+            
+        }
+    }
+}
---
+++ TestScripting/SeleniumTestA/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumTestA")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumTestA")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ec5d523f-3b1e-413d-929d-d9d550477b86")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
